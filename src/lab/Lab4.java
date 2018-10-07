a1a7d7ff3188ab0a23cdf2661d7654a1f9d6a6cc
Yannick <yannick.dorn@gmail.com>
2018-10-07 19:36:48 -0400

    Fuck Java

--- src/lab/Lab4.java
+++ src/lab/Lab4.java
@@ -1,14 +1,18 @@
 package lab;
+
 public class Lab4 {
 	public static void test(String[] args) {
 		System.out.println(isPalindrome("racecar"));
 	}
 	public static boolean isPalindrome(String str) {
-		//Placing a comment here
-		char left, right;
-		while (!str.isEmpty()) {
-			str = str.substring(1,str.length() - 1);
-		}
-		return true;
+		System.out.println(str.length());
+		if (str.length() == 1) return true;
+		char left = Character.toLowerCase(str.charAt(0));
+		char right = Character.toLowerCase(str.charAt(0));
+		if (left == right) isPalindrome(str.substring(1, str.length() - 1));
+		return false;
+
+
+		
 	}
 }
